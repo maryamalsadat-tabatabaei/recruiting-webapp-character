4f2da43d2d0d823a0ae7ef8f6af0e470801efc54
Maryam (Maryamalsadat) Tabatabaei <87692864+maryamsadat-tabatabaei@users.noreply.github.com>
2023-09-27 21:19:28 -0400

    Save the characterto the API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AttributeList from "./components/attributes/AttributeList";
 import ClassList from "./components/classes/ClassList";
 import SkillList from "./components/skills/SkillList";
 import "./App.css";
+
+const API_BASE_URL = "https://recruiting.verylongdomaintotestwith.ca/api";
 
 function App() {
   const [attributes, setAttributes] = useState({
@@ -18,12 +20,57 @@ function App() {
     setAttributes(newAttributes);
   };
 
+  const saveCharacterData = async () => {
+    try {
+      const response = await fetch(
+        `${API_BASE_URL}/{maryamalsadat-tabatabaei}/character`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(attributes),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to save character data");
+      }
+    } catch (error) {
+      console.error("Error saving character data:", error);
+    }
+  };
+
+  const retrieveCharacterData = async () => {
+    try {
+      const response = await fetch(
+        `${API_BASE_URL}/{maryamalsadat-tabatabaei}/character`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        updateAttributes(data.body);
+      } else {
+        throw new Error("Failed to retrieve character data");
+      }
+    } catch (error) {
+      console.error("Error retrieving character data:", error);
+    }
+  };
+
+  useEffect(() => {
+    retrieveCharacterData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
       <h2>Character</h2>
+      <div>
+        <button onClick={saveCharacterData}>Save character</button>
+      </div>
       <section className="App-section">
         <div>
           <h1>Attributes</h1>
